b5e9476a306aa5aa4899933bc13fac0a26d35757
dariaselezneva1 <cool.swampie@bk.ru>
2026-06-20 21:48:24 +0300

    Static review build — subsidy on home, RUDN numbers, contacts, faculty photos, partner logos

---
+++ _next/static/chunks/app/not-found-309bab8d01492750.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,27,989,364,839,935],{7134:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=7134)}),_N_E=n.O()}]);
